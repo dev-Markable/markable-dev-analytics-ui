a0b60318a2cd154887c9bf074bc9aab1f3e8c148
Osechinsky, Boris <boris.osechinskiy@x5.ru>
2026-05-25 11:16:21 +0300

    drop before refactoring

--- src/component/WeeklyStats/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as WeeklyTabs } from './WeeklyTabs.jsx';
-export { default as WeeklyStatsCards } from './WeeklyStatsCards.jsx';
-export { default as WeeklyUsersTable } from './WeeklyUsersTable.jsx';
